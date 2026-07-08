058d9f9db0380a1df4eba88e4e3a874f9417b6ae
Umair Shabbir | Aspiring AI Engineer <umairshabbir415@gmail.com>
2026-07-09 01:35:38 +0500

    Update day03.py

--- 01-python-advanced/day03.py
+++ 01-python-advanced/day03.py
@@ -13,3 +13,10 @@ even = lambda x: x % 2 == 0
 
 print(even(10))
 print(even(7))
+
+// Square Every Number by map
+numbers = [1, 2, 3, 4, 5]
+
+squares = list(map(lambda x: x ** 2, numbers))
+
+print(squares)
