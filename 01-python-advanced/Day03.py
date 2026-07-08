cb2f10a69a7f5b73dbad1a8391882c56b481b77f
Umair Shabbir | Aspiring AI Engineer <umairshabbir415@gmail.com>
2026-07-09 01:33:23 +0500

    Create Day03.py

---
+++ 01-python-advanced/Day03.py
@@ -0,0 +1,10 @@
+// Add Two Numbers By lambda fuction 
+add = lambda a, b: a + b
+
+print(add(10, 20))
+
+// Maximum Number by lamba functions
+maximum = lambda x, y: x if x > y else y
+
+print(maximum(15, 30))
+
