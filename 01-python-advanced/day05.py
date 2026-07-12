efd47a87d565f611454f67b7e26ab6474a67df64
Umair Shabbir | Aspiring AI Engineer <umairshabbir415@gmail.com>
2026-07-13 02:01:25 +0500

    Update day05.py

--- 01-python-advanced/day05.py
+++ 01-python-advanced/day05.py
@@ -1,1 +1,19 @@
 
+from functools import wraps
+import time
+
+def timer(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.perf_counter()
+        result = func(*args, **kwargs)
+        end = time.perf_counter()
+        print(f"{func.__name__} executed in {end-start:.6f} seconds")
+        return result
+    return wrapper
+
+@timer
+def calculate():
+    return sum(i*i for i in range(10_000_000))
+
+calculate()
