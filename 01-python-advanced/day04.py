feb595978b52f5f1fc328948fa550b249a3e9aa3
Umair Shabbir | Aspiring AI Engineer <umairshabbir415@gmail.com>
2026-07-13 02:01:52 +0500

    Update day04.py

--- 01-python-advanced/day04.py
+++ 01-python-advanced/day04.py
@@ -1,1 +1,17 @@
 
+import threading
+import time
+
+def worker(name):
+    for i in range(5):
+        print(f"{name}: {i}")
+        time.sleep(1)
+
+t1 = threading.Thread(target=worker, args=("Thread-1",))
+t2 = threading.Thread(target=worker, args=("Thread-2",))
+
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
